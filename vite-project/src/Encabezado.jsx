8bfe06f131077c989641c1bc24d28d3f0e3790ef
thiagoHerrera <tititomash@gmail.com>
2025-08-18 21:39:21 -0300

    m

---
+++ vite-project/src/Encabezado.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Encabezado = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Encabezado
---
+++ vite-project/src/Futer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Futer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Futer
